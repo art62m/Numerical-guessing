7f2df330d1d442e8c389faeed97794bf858a447f
Artem <art62.rzn@gmail.com>
2021-08-11 15:48:01 +0300

    added main functional

--- ng.py
+++ ng.py
@@ -1,11 +1,27 @@
 from random import *
+from typing import ItemsView
 
 #Функция для проверки корректности введенных данных
-def is_valid(user_num): 
-    if 1 <= int(user_num) <= 100:
+def is_valid(num): 
+    if 1 <= num <= 100:
         return True
     else:
         return False
 
 rand_num = randint(1,100)
 print("Добро пожаловать в числовую угадайку")
+
+#Основной цикл программы
+while True:
+    user_num = int(input())
+    if not is_valid(user_num):
+        print("А может быть все-таки введем целое число от 1 до 100?")
+    else:
+        if user_num < rand_num:
+            print("Ваше число меньше загаданного, попробуйте еще разок")
+        elif user_num > rand_num:
+            print("Ваше число больше загаданного, попробуйте еще разок")
+        else:
+            print("Вы угадали, поздравляем!")
+            break
+print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
