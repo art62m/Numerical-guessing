5e74f41867aa07af51e1c0ed9028bcc606c98ba3
Artem <art62.rzn@gmail.com>
2021-08-11 15:34:04 +0300

    Add func checking data

--- ng.py
+++ ng.py
@@ -1,3 +1,11 @@
 from random import *
+
+#Функция для проверки корректности введенных данных
+def is_valid(user_num): 
+    if 1 <= int(user_num) <= 100:
+        return True
+    else:
+        return False
+
 rand_num = randint(1,100)
 print("Добро пожаловать в числовую угадайку")
